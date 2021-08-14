f3679e7d5e41c97c216ece83256e3f2cdc1ebeee
Richard Allaway <richard.allaway@gmail.com>
2021-08-14 19:55:43 -0400

    reactive api service: center and distance inputs, location$ is output

--- locations/src/app/api.service.ts
+++ locations/src/app/api.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LatLngLiteral } from 'leaflet';
+import { combineLatest, Subject } from 'rxjs';
+import { map, scan, switchMap } from 'rxjs/operators';
 import { SchoolLocation } from './school-location';
+
+type Query = { distanceInMiles: number, latlng: LatLngLiteral }
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +14,28 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  center = new Subject<LatLngLiteral>();
+  distanceInMiles = new Subject<number>();
+
+  location$ = combineLatest([
+    this.distanceInMiles.pipe(
+      map(distanceInMiles => <Query>{ distanceInMiles }),
+    ),
+    this.center.pipe(
+      map(latlng => <Query>{ latlng }),
+    ),
+  ]).pipe(
+    scan((acc, query) => { return {...acc, ...query } }, {} as Query),
+    switchMap(query => this.findLocationsByDistance(query)),
+  );
+
   private headers = new HttpHeaders({
     'Content-Type':  'application/json'
   });
 
-  findLocationsByDistance(distanceInMiles: number, latlng: { lat: number, lng: number }) {
-    return this.http.post<SchoolLocation[]>(`http://localhost:3000/rpc/distances?distance=lt.${distanceInMiles}&order=distance`,
-      latlng,
+  private findLocationsByDistance(query: Query) {
+    return this.http.post<SchoolLocation[]>(`http://localhost:3000/rpc/distances?distance=lt.${query.distanceInMiles}&order=distance`,
+      query.latlng,
       {
         headers: this.headers
       }
